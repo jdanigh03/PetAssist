e883ac1b27b4e764d4c79403fc931eeca11bf64d
FreddySoto123 <orlandes259@gmail.com>
2024-11-14 08:47:53 -0400

    cambiow

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -225,7 +225,7 @@
                 @endif
             @else
                 <a href="/" class="{{ request()->is('/') ? 'active' : '' }}">Inicio</a>
-                <a href="/contactos" class="{{ request()->is('contactos') ? 'active' : '' }}">Contactos</a>
+                
             @endif
         </nav>
 
