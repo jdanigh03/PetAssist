9372f2e337b9795956b794b2e63c747995ac31a1
Ferchox <fernandotriguero123@gmail.com>
2024-11-28 08:21:04 -0400

    correccion estilo mascota

--- resources/views/mascotas.blade.php
+++ resources/views/mascotas.blade.php
@@ -29,7 +29,7 @@
             border-radius: 8px;
             min-width: 350px;
             max-width: 600px;
-            box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1);
+            box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
             border: none;
             text-align: center;
             margin-bottom: 20px;
@@ -72,6 +72,9 @@
             margin-top: 20px;
             border-radius: 8px;
             margin-bottom: 20px;
+            min-width: 350px;
+            max-width: 600px;
+            box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
         }
 
         .foto-container {
