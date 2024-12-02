c268844fbba8765fd55f68bb149e634353ef9537
Ferchox <fernandotriguero123@gmail.com>
2024-12-02 14:30:41 -0400

    corrigiendo rutas

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/petshop', function () {
     return view('petshop');
 });
 
-Route::get('/control-citas', [CitaController::class, 'mostrarHistorial'])->name('citas.historial');
+Route::get('/control-citas', [CitaController::class, 'mostrarHistorial'])->name('citas.control');
 Route::get('/historial-citas', [CitaController::class, 'historialCitas'])->name('citas.historial');
 Route::get('/controldemascotas', [MascotaController::class, 'todasMascotas'])->name('mascotas.todas'); 
 
@@ -177,9 +177,7 @@ Route::get('/producto/{id}', [ProductController::class, 'verProducto'])->name('p
 
 Route::get('/generar-reporte', [CitaController::class, 'generarReportes'])->name('admin.generarReportes');
 
-Route::post('/generar-pdf', [CitaController::class, 'generarPDF'])->name('admin.generarPDF');
-
-Route::post('/generar-reporte', [CitaController::class, 'generarReportes'])->name('admin.generarReportes');
+Route::post('/generar-reporte', [CitaController::class, 'generarReportes'])->name('admin.generarPDF');
 
 Route::get('/movimientos', [ProductController::class, 'verMovimientos'])->name('productos.movimientos');
 // Ruta para la vista de control de mascotas
